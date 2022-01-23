bfafd26995cf169316452b6a5b3a43c87fed64c3
MaxCazares <maxocazares@hotmail.com>
2022-01-22 21:12:22 -0600

    bugs corregidos

--- src/helper/getGifs.js
+++ src/helper/getGifs.js
@@ -1,19 +1,22 @@
 const getGifs = async (category) => {
-    const api_key = 'olker8uXsi4QXnEirqeLJiSG5rRrhjDy'    
-    const url = `http://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=${api_key}`
-    
-    const respuesta = await fetch(url);
-    const {data} = await respuesta.json();
-    
-    const gifs = data.map(img => {
-        return {
-            id: img.id,
-            title: img.title,
-            url: img.images?.downsized_medium.url
-        }
-    });
-    
-    // console.log(gifts);
+    const api_key = 'olker8uXsi4QXnEirqeLJiSG5rRrhjDy';
+    const url = `http://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=${api_key}`;
+
+    try {
+        const respuesta = await fetch(url);
+        const { data } = await respuesta.json();
+
+        const gifs = data.map(img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
     return gifs;
 };
 
